18afeea8569aa1e7ac587056ea430461c46168fb
Vinicius Pereira <vguard.2003@gmail.com>
2023-01-09 09:16:25 -0300

    Created form, services and tool

--- public/script/main.js
+++ public/script/main.js
@@ -1,20 +1,34 @@
 (function () {
-    var menu = document.getElementById('scrollMenu')
-    var buttonMenu = document.getElementById('btn')
+    var bgHeader = document.getElementById('bg-header')
     var logo = document.getElementById('logo')
+    var textLogo = document.getElementById('text-logo')
+    var navMenu1 = document.getElementById('list-nav')
+    var textA = document.getElementById('a-text')
+    var header = document.getElementById('header')
+
+
 
     window.addEventListener('scroll', function () {
         if (window.scrollY > 0) {
-            menu.classList.add('menuFixo')
-            buttonMenu.classList.add('shadow-black')
-            logo.src = "/public/assets/logo-white.svg"
-            
-        } else { 
-            menu.classList.remove('menuFixo');
-            buttonMenu.classList.remove('shadow-black')
-            buttonMenu.classList.add('shadow-white')
-            logo.src = "/public/assets/logo.svg"
+            bgHeader.classList.add('bg-dark')
+            navMenu1.classList.add('d-none')
+            textA.classList.remove('col-md-3')
+            textA.classList.add('col-md-6')
+            header.classList.remove('mb-4')
 
+
+            logo.src = "/public/assets/logo-white.svg"
+            textLogo.textContent = 'Conectando negócios a quem precisa da solução. Vamos juntos?'
+
+        } else { 
+            bgHeader.classList.remove('bg-dark')
+            logo.src = "/public/assets/logo.svg"
+            textLogo.textContent = ''
+            navMenu1.classList.remove('d-none')
+
+            textA.classList.remove('col-md-6')
+            textA.classList.add('col-md-3')
     }
     });
 })();
+
