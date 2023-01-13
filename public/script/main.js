f5ce0460afa57a530522249eb8cee28cb849bb63
Vinicius Pereira <vguard.2003@gmail.com>
2023-01-13 13:12:51 -0300

    created and add background mobile, resolved bugs and modified spacing additional

--- public/script/main.js
+++ public/script/main.js
@@ -8,6 +8,8 @@
     var textli = document.getElementById('text-li')
     var textli2 = document.getElementById('text-li2')
     var textli3 = document.getElementById('text-li3')
+    var textli4 = document.getElementById('text-li4')
+
 
 
 
@@ -16,6 +18,8 @@
             textli.classList.replace('text-dark','text-light')
             textli2.classList.replace('text-dark','text-light')
             textli3.classList.replace('text-dark','text-light')
+            textli4.classList.replace('text-dark','text-light')
+
 
             bgHeader.classList.add('bg-dark')
             navMenu1.classList.add('d-none', 'd-lg-block')
@@ -27,16 +31,17 @@
 
 
             logo.src = "/public/assets/logo-white.svg"
-            // textLogo.textContent = 'Conectando negócios a quem precisa da solução. Vamos juntos?'
+            textLogo.textContent = 'Conectando negócios a quem precisa da solução. Vamos juntos?'
 
         } else { 
             textli.classList.replace('text-light','text-dark')
             textli2.classList.replace('text-light','text-dark')
             textli3.classList.replace('text-light','text-dark')
+            textli4.classList.replace('text-light','text-dark')
 
             bgHeader.classList.remove('bg-dark')
             logo.src = "/public/assets/logo.svg"
-            // textLogo.textContent = ''
+            textLogo.textContent = ''
             navMenu1.classList.remove('d-sm-block')
             navMenu1.classList.add('d-md-flex')
 
